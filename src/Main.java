3b517deb9fcfbe180cce089730bc47b196f287ba
victorcesar2 <vitor.c.j@hotmail.com>
2023-11-27 21:43:59 -0300

    inicio-do-desafio

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String nomeCliente = "Claudio Ramos";
+        String tipoConta = "Conta Corrent";
+        double saldo = 5000.00;
+
+        System.out.println("**************************");
+        System.out.println("Cliente: "+ nomeCliente);
+        System.out.println("Tipo da Conta: " + tipoConta);
+        System.out.println("Saldo Dísponivel: " + saldo);
+        System.out.println("**************************");
+
+        
+    }
+}
